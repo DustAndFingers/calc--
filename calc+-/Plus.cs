20b3e5be0fe11a9c98ccc96a61d29ff7be2d6c18
Jak Fresko <maximkovolkoff@gmail.com>
2021-09-24 11:23:31 +0300

    Add project files.

---
+++ calc+-/Plus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc__
+{
+    class Plus
+    {
+        public int plus(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+    }
+}
---
+++ calc+-/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Складываем первое число:");
+            string plusfirst = Console.ReadLine();
+            int plusfirs;
+            plusfirs = Convert.ToInt32(plusfirst);
+            Console.WriteLine("Складываем второе число:");
+            string plussecond = Console.ReadLine();
+            int plussecon;
+            plussecon = Convert.ToInt32(plussecond);
+            Plus Slojenie = new Plus();
+            int resultPlus = Slojenie.plus(plusfirs, plussecon);
+            Console.WriteLine("\n результат сложения:" + resultPlus);
+            Console.ReadKey();
+
+
+        }
+    }
+}
